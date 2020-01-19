7ef8c8b55b2e9f0024e2cd32e5e28b9b50b67006
ArtemichNed <rabotayvit@gmail.com>
2020-01-19 19:28:40 +0300

    Add Lab_07 Graph DFS and BFS

---
+++ Lab_07/Lab_07/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Lab_07
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 23);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(315, 318);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 377);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(337, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 420);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(337, 22);
+            this.textBox4.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(353, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 47);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Deapth first search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(353, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 51);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Breadth first seach";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(353, 170);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 482);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Lab_07/Lab_07/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace Lab_07
+{
+    public partial class Form1 : Form
+    {
+        static int n = 19;
+        int[,] matrix = new int[n, n];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] lines = File.ReadAllLines(@"matrix2.txt");
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] temp = lines[i].Split(' ');
+                for (int j = 0; j < temp.Length; j++)
+                    matrix[i, j] = Convert.ToInt32(temp[j]);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    textBox1.Text += matrix[i, j].ToString() + "   ";
+                }
+                textBox1.Text += "\r\n";
+            }
+            //Осуществляем поиск в глубину
+            textBox3.Clear();
+            int u = int.Parse(textBox2.Text);
+            var result = DFS(u, matrix);
+            foreach (int i in result)
+            {
+                { textBox3.Text += i.ToString() + " "; }
+            }
+        }
+        private HashSet<int> DFS(int index, int[,] matrix)
+        {
+            var list = new HashSet<int>();
+            list.Add(index);
+            for (int j = 0; j < matrix.GetLength(0); j++)
+            {
+                if (matrix[index, j] == 1)
+                {
+                    matrix[index, j] = matrix[j, index] = 0;
+                    list.UnionWith(DFS(j, matrix));
+                }
+            }
+            return list;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] lines = File.ReadAllLines(@"matrix2.txt");
+            // int[,]  C = new int[lines.Length, lines[0].Split(' ').Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] temp = lines[i].Split(' ');
+                for (int j = 0; j < temp.Length; j++)
+                    matrix[i, j] = Convert.ToInt32(temp[j]);
+            }
+            textBox4.Clear();
+            Queue<int> q = new Queue<int>();    //Это очередь для вершин
+            //int n = 19;
+            int u = int.Parse(textBox2.Text);
+            bool[] used = new bool[n];  //массив с посещенными вершинами
+            List<int> list = new List<int>();
+            list.Add(u);
+            used[u] = true;
+            q.Enqueue(u);
+            while (q.Count != 0)
+            {
+                u = q.Peek();
+                q.Dequeue();
+
+                for (int i = 0; i < 19; i++)
+                {
+                    if (Convert.ToBoolean(matrix[u, i]))
+                    {
+                        if (!used[i])
+                        {
+                            used[i] = true;
+                            list.Add(i);
+                            q.Enqueue(i);
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < list.Count; i++) textBox4.Text += list[i].ToString() + " ";
+        }
+    }
+}
