5d1d719754455b9cb09e2957ce43ab4287471bfc
ArtemichNed <rabotayvit@gmail.com>
2020-01-19 19:10:12 +0300

    Add Lab_06 Bulk caving system

---
+++ Lab_06/Lab_06/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Lab_06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(14, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 323);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(14, 358);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(196, 236);
+            this.textBox5.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(372, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "15";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(372, 98);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.Text = "2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(216, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 17);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Общее кол-во заявок";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(221, 98);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(145, 34);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Макс. кол-во заявок \r\nв очереди";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(372, 137);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 198);
+            this.textBox3.TabIndex = 17;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(372, 369);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 18;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(372, 412);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 19;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(372, 454);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 22);
+            this.textBox8.TabIndex = 20;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(372, 499);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 22);
+            this.textBox9.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(221, 369);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 17);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Кол-во отказов 1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(221, 412);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 17);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Кол-во отказов 2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(216, 454);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 34);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Среднее кол-во\r\nзаявок в очереди 1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(216, 499);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 34);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Среднее кол-во \r\nзаявок в очереди 2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(372, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 26;
+            this.button1.Text = "Count";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 629);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Lab_06/Lab_06/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_06
+{
+    enum request_type //перечисляем типы операций
+    {
+        ACNT, //узнать счет
+        XCHG, //обмен валюты
+        CRED, //выдать кредит
+        CARD, //сделать карту
+        NONE
+    };
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(textBox2.Text); //общее количество заявок
+            List<Request> list_client = new List<Request>();
+            int ID = 1;
+            Random rnd = new Random();
+            request_type operation = request_type.NONE;
+            int start_time;
+            int service_time = 0;
+            int timer = 20;
+            int dec = 0;
+            int tot_reject_XCHG = 0;    //количество отказов первого типа
+            int tot_reject_ACNT = 0;    //количетво отказов второго типа
+            double tot_queue_count1 = 0;
+            double tot_queue_count2 = 0;
+            double avr_queue_count1 = 0;
+            double avr_queue_count2 = 0;
+            //генерируем заявки
+            for (int i = 0; i < n; i++)
+            {
+                int type = rnd.Next(4);
+                switch (type)
+                {
+                    case 0:
+                        operation = request_type.ACNT;
+                        dec = rnd.Next(-10, 10);
+                        service_time = set_service_time(operation, dec);
+                        break;
+                    case 1:
+                        operation = request_type.CARD;
+                        dec = rnd.Next(-10, 10);
+                        service_time = set_service_time(operation, dec);
+                        break;
+                    case 2:
+                        operation = request_type.CRED;
+                        dec = rnd.Next(-10, 10);
+                        service_time = set_service_time(operation, dec);
+                        break;
+                    case 3:
+                        operation = request_type.XCHG;
+                        dec = rnd.Next(-10, 10);
+                        service_time = set_service_time(operation, dec);
+                        break;
+                }
+                start_time = timer + rnd.Next(-10, 10);
+                list_client.Add(new Request(ID, start_time, service_time, true, operation));
+                ID++;
+                timer += 5;
+            }
+            foreach (Request client in list_client)
+            {
+                textBox1.Text += client.ID + " " + client.operation + " " + client.start_time + " " + "\r\n";
+            }
+            //создаем кассы
+            ID = 1;
+            List<Kassa> list_kassa = new List<Kassa>(7);
+            for (int i = 0; i < 7; i++)
+            {
+                if (i == 0 || i == 1)
+                { list_kassa.Add(new Kassa(ID, kassa_type.N12)); ID++; }
+                else
+                { list_kassa.Add(new Kassa(ID, kassa_type.N37)); ID++; }
+            }
+            foreach (Kassa kassir in list_kassa)
+            {
+                textBox3.Text += kassir.state + " " + kassir.k_type + "\r\n";
+            }
+            //создаем очереди для заявок
+            int q_volume = int.Parse(textBox4.Text);
+            Queue<Request> q12 = new Queue<Request>(q_volume);
+            Queue<Request> q37 = new Queue<Request>(q_volume);
+
+            List<Request> list_ready = new List<Request>(n);
+            int global_time = 0;
+            ///*
+
+            while (list_ready.Count != n)
+            {
+                //освобождаем кассы
+                foreach (Kassa kassir in list_kassa)
+                {
+                    if (kassir.state == operator_state.Occuped && (kassir.cl.start_time + kassir.cl.service_time) == global_time)
+                    {
+                        list_ready.Add(kassir.cl);
+                        kassir.cl = null;
+                        kassir.state = operator_state.Vacant;
+                    }
+                }
+                //добавляем из очередей новые заявки в свободные кассы
+                foreach (Kassa kassir in list_kassa)
+                {
+                    if (kassir.state == operator_state.Vacant && kassir.k_type == kassa_type.N12 && q12.Count != 0)
+                    {
+                        kassir.cl = q12.Peek();
+                        kassir.state = operator_state.Occuped;
+                        q12.Dequeue();
+                    }
+                    if (kassir.state == operator_state.Vacant && kassir.k_type == kassa_type.N37 && q37.Count != 0)
+                    {
+                        kassir.cl = q37.Peek();
+                        kassir.state = operator_state.Occuped;
+                        q37.Dequeue();
+                    }
+
+                }
+                foreach (Request client in list_client)
+                {
+                    if (client.start_time == global_time)
+                    {
+                        foreach (Kassa kassir in list_kassa)
+                        {
+                            if (kassir.state == operator_state.Vacant && kassir.k_type == kassa_type.N12 && client.operation == request_type.XCHG)
+                            {
+                                kassir.cl = client;
+                                kassir.state = operator_state.Occuped;
+                                break;
+                            }
+
+                            if (kassir.state == operator_state.Vacant && kassir.k_type == kassa_type.N37)
+                            {
+                                if (client.operation == request_type.ACNT || client.operation == request_type.CARD || client.operation == request_type.CRED)
+                                {
+                                    kassir.cl = client;
+                                    kassir.state = operator_state.Occuped;
+                                    break;
+                                }
+                            }
+
+                            if (kassir.state == operator_state.Occuped && kassir.ID == 2 && client.operation == request_type.XCHG)
+                            {
+                                if (q12.Count == q_volume)
+                                {
+                                    client.request_state = false;
+                                    list_ready.Add(client);
+                                    tot_reject_XCHG++;
+                                }
+                                else q12.Enqueue(client);
+                                break;
+                            }
+                            if (kassir.state == operator_state.Occuped && kassir.ID == 7)
+                            {
+                                if (client.operation == request_type.ACNT || client.operation == request_type.CARD || client.operation == request_type.CRED)
+                                {
+                                    if (q37.Count == q_volume)
+                                    {
+                                        client.request_state = false;
+                                        list_ready.Add(client);
+                                        tot_reject_ACNT++;
+                                    }
+                                    else q37.Enqueue(client);
+                                }
+                            }
+                        }
+                    }
+                }
+                tot_queue_count1 += q12.Count;
+                tot_queue_count2 += q37.Count;
+                global_time++;
+            }
+            //  */
+            foreach (Request client in list_ready)
+            {
+                textBox5.Text += client.ID + " " + client.operation + " " + client.start_time + " " + client.service_time + " " + client.request_state + "\r\n";
+            }
+            textBox6.Text = tot_reject_XCHG.ToString();
+            textBox7.Text = tot_reject_ACNT.ToString();
+
+            avr_queue_count1 = Math.Round(tot_queue_count1 / global_time, 3);
+            textBox8.Text = avr_queue_count1.ToString();
+            avr_queue_count2 = Math.Round(tot_queue_count2 / global_time, 3);
+            textBox9.Text = avr_queue_count2.ToString();
+            textBox3.Text = global_time.ToString();
+        }
+        private int set_service_time(request_type operation, int dec)
+        {
+            int service_time = 0;
+            if (operation == request_type.ACNT)
+                service_time = 30 + dec;
+            else if (operation == request_type.XCHG)
+                service_time = 50 + dec;
+            else if (operation == request_type.CARD)
+                service_time = 80 + dec;
+            else
+                service_time = 60 + dec;
+            return service_time;
+        }
+    }
+    class Request
+    {
+        public int ID; //номер заявки
+        public int start_time; //время создания заявки
+        public int service_time; //время обслуживания заявки
+        public int stop_time; //время завершения заявки
+        public bool request_state; //состояние заявки
+        public readonly request_type operation; //тип операции
+
+        //private Random dec; //отклонение от среднего времени обработки заявки
+
+        public Request(int _ID, int _start_time, int _service_time, bool _request_state, request_type _operation)
+        {
+            ID = _ID;
+            start_time = _start_time;
+            service_time = _service_time;
+            request_state = _request_state;
+            operation = _operation;
+        }
+    }
+    enum kassa_type
+    {
+        N12, //кассы ХСНG
+        N37, //кассы ACNT, CRED, CARD
+        N    //  
+    }
+    enum operator_state //состояние кассы
+    {
+        Vacant, //свободна
+        Occuped, //занята
+        NotReady //завершила работу, но еще не освободилась
+    }
+    class Kassa
+    {
+        public int ID;
+        public kassa_type k_type; //тип кассы
+        public operator_state state; //состояние
+        private int client_id; //id клиента
+        public new Request cl; //клиент
+        public Kassa(int _ID, kassa_type _k_type)
+        {
+            ID = _ID;
+            k_type = _k_type;
+            state = operator_state.Vacant;
+            //cl = new Request();
+            cl = null;
+        }
+    }
+}
